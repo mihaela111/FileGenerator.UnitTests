e29e5fb45e67a371f60e25e37e98a13f3f9f4e08
mihaela pasca <mihaela.pasca@ulbsibiu.ro>
2023-07-03 00:21:23 +0300

    Unit Test project

---
+++ CompositeTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsFormsApp1.UnitTests
+{
+    [TestClass]
+    public class CompositeTests
+    {
+        [TestMethod]
+        public void SetText_validInput_returnExpectedString()
+        {
+            string ExpectedString = "TestComposite";
+            Composite composite = new Composite("TestComposite");
+
+
+            string actualString = composite.ReturnText();
+            Assert.AreEqual(ExpectedString, actualString);
+
+
+        }
+    }
+}
+
+
+
+
---
+++ LeafTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WindowsFormsApp1.UnitTests
+{
+    [TestClass]
+    public class LeafTests
+    {
+    [TestMethod]
+    public void SetText_validInput_returnExpectedString()
+        {
+            string ExpectedString = "TesSetTextLeaf";
+            Leaf leaf = new Leaf("TestLeaf");
+            leaf.SetText("TesSetTextLeaf");
+
+            string actualString = leaf.ReturnText();
+            Assert.AreEqual(ExpectedString, actualString);
+
+
+        }
+
+
+
+        [TestMethod]
+        public void ReturnText_validInput_returnExpectedString()
+        {
+            string ExpectedString = "TestLeaf";
+            Leaf leaf = new Leaf("TestLeaf");
+;
+
+            string actualString = leaf.ReturnText();
+            Assert.AreEqual(ExpectedString, actualString);
+
+
+        }
+    }
+}
